4555cd5f23c4c8730484629b634a9254c1eaf8a9
Gia Bao Nguyen <giabao.nguyen@sage.com>
2020-12-11 13:39:16 -0800

    add xml parser

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
-let xmlparser = require('express-xml-bodyparser');
+let xmlparser = require('@type/express-xml-bodyparser');
 app.use(xmlparser());
 
 app.post('/api/vendor', function (req, res) {
