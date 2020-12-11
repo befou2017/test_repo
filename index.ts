2e16766a24fe5e66e2971ac5baae93d4c022f6b0
Gia Bao Nguyen <giabao.nguyen@sage.com>
2020-12-11 13:25:51 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
-
+let xmlparser = require('express-xml-bodyparser');
+app.use(xmlparser());
 
 app.post('/api/vendor', function (req, res) {
    console.log(`PORT : ${PORT}</p>`);
@@ -11,6 +12,8 @@ app.post('/api/vendor', function (req, res) {
    console.log(`req.user : ${JSON.stringify(req.baseUrl)}`);
    console.log(`req.query : ${JSON.stringify(req.query)}`);
    console.log(`req.headers : ${JSON.stringify(req.headers)}`);
+   
+   res.send(`<h1>Welcome to your simple server! Awesome right</h1> ${JSON.stringify(req.params,null,1)}`);
 });
 
 app.use("*",(req, res) =>{
