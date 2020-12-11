0822b6cec1b11b1592d96111577924f04734ff62
Gia Bao Nguyen <giabao.nguyen@sage.com>
2020-12-11 14:35:38 -0800

    vendorID

--- index.ts
+++ index.ts
@@ -4,11 +4,20 @@ const PORT : string|number = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
 
-app.use(bodyParser.xml());
+app.use(bodyParser.xml({
+   limit: '1MB',   // Reject payload bigger than 1 MB
+   xmlParseOptions: {
+     normalize: true,     // Trim whitespace inside text nodes
+     normalizeTags: true, // Transform tags to lowercase
+     explicitArray: false // Only put nodes in array if >1
+   }
+ }));
 
 app.post('/api/vendor', function (req, res) {
    console.log(`req.body : ${JSON.stringify(req.body)}`);
    console.log(`req.body : ${req.body}`);
+   console.log(`req.body.vendorID : ${req.body.vendorID}`);
+   console.log(req.body);
    console.log(`req.headers : ${JSON.stringify(req.headers)}`);
    
    res.send(`<h1>Welcome to your simple server! Awesome right</h1> ${JSON.stringify(req.params,null,1)}`);
